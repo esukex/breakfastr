6882fd85e9174daec6e3818a3e312d02833d622b
Jackson Powell <esukex@gmail.com>
2015-03-05 15:52:58 +0000

    reset session for security

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			flash[:success]="User added!"
+			reset_session
+			session[:user_id]=@user.id
 			redirect_to root_path
 		else
 			flash[:error]="Oops, try again."
