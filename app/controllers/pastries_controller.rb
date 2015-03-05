f03adccd2eaf39df5d8f5818aea33394dd891b05
Jackson Powell <esukex@gmail.com>
2015-03-05 12:22:35 +0000

    Created index in Pastries Controller

--- app/controllers/pastries_controller.rb
+++ app/controllers/pastries_controller.rb
@@ -1,13 +1,29 @@
 class PastriesController < ApplicationController
-  def index
+###########################################################################################################
+	def index
+		@pastries = [
+									{
+										name:"Croissant",
+										price_in_pence:350,
+										description:"Butter pastry croissant"
+									},
+									{
+										name:"Pan au Chocolat",
+										price_in_pence:400,
+										description:"Butter pastry with a chocolate filling"
+									}
+								]
   end
-
+###########################################################################################################
   def show
+		
   end
-
+###########################################################################################################
   def new
+		
   end
 
   def edit
+		
   end
 end
