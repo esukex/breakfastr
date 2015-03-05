fe2300d17a6dad9e51446db929a57717a86aebd1
Jackson Powell <esukex@gmail.com>
2015-03-05 15:48:09 +0000

    create user new

--- app/controllers/pastries_controller.rb
+++ app/controllers/pastries_controller.rb
@@ -1,29 +1,55 @@
 class PastriesController < ApplicationController
 ###########################################################################################################
 	def index
-		@pastries = [
-									{
-										name:"Croissant",
-										price_in_pence:350,
-										description:"Butter pastry croissant"
-									},
-									{
-										name:"Pan au Chocolat",
-										price_in_pence:400,
-										description:"Butter pastry with a chocolate filling"
-									}
-								]
+		@pastries = Pastry.all
   end
 ###########################################################################################################
   def show
-		
+		@pastry=Pastry.find(params[:id])
   end
 ###########################################################################################################
   def new
-		
+		@pastry = Pastry.new
   end
-
+	
+	def create
+		@pastry = Pastry.new(pastry_params)
+		if @pastry.save
+			flash[:success]="Pastry added!"
+			redirect_to root_path
+		else
+			flash[:error]="Oops, try again."
+			render :new
+		end
+	end
+###########################################################################################################	
   def edit
-		
+		@pastry = Pastry.find(params[:id])
   end
+	
+	def update
+		@pastry = Pastry.find(params[:id])
+		if @pastry.update(pastry_params)
+			flash[:success]="Pastry updated!"
+			redirect_to root_path
+		else
+			flash[:error]="Oops, try again."
+			render :edit
+		end
+	end
+###########################################################################################################
+	def destroy
+		pastry = Pastry.find(params[:id])
+		pastry.destroy
+		flash[:success]="Entry Deleted"
+		redirect_to root_path
+	end
+###########################################################################################################
+#---------------------------------------------------------------------------------------------------------#
+###########################################################################################################
+	private
+	def pastry_params
+		params.require(:pastry).permit(:name, :price_in_pence, :description)
+	end
+###########################################################################################################
 end
