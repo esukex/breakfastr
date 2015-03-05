feae8b1a2addc37f4592f1f4e342e8445548bae7
Jackson Powell <esukex@gmail.com>
2015-03-05 15:19:22 +0000

    Generate User Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150305123645) do
+ActiveRecord::Schema.define(version: 20150305151610) do
 
   create_table "pastries", force: true do |t|
     t.string   "name"
     t.integer  "price_in_pence"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
