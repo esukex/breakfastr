6e9726274b2dde44cac0b52e91a4c2b13150afab
Jackson Powell <esukex@gmail.com>
2015-03-05 12:52:12 +0000

    Seeded DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Pastry.create([
+	{
+		name:"Croissant",
+		price_in_pence:350,
+		description:"Butter pastry croissant"
+	},
+	{
+		name:"Pan au Chocolat",
+		price_in_pence:400,
+		description:"Butter pastry with a chocolate filling"
+	}
+])
