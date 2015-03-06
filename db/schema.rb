6b3e82558d28cf60898b1a6e27daaa0f507fff68
Jackson Powell <esukex@gmail.com>
2015-03-06 10:07:42 +0000

    migrate db for id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150305151610) do
+ActiveRecord::Schema.define(version: 20150305170023) do
 
   create_table "pastries", force: true do |t|
     t.string   "name"
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 20150305151610) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "pastries", ["user_id"], name: "index_pastries_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "username"
